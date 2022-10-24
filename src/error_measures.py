901e4d8d3437df293c6e5e3cf337dafb30e8225c
apiekarz <a.piekarzewicz@stud.elka.pw.edu.pl>
2022-10-24 09:44:42 +0200

    Functions for MSE, RMSE, MAE, Index of Agreement

--- src/error_measures.py
+++ src/error_measures.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 
 
@@ -12,3 +13,45 @@ def get_accuracy(model, loader, device, pct_close=0.2):
         correct += (torch.abs(pred-targets.view_as(pred)) <= torch.abs(pct_close * targets.view_as(pred))).sum().item()
         total += data.shape[0]
     return correct / total
+
+
+def get_error_measures(model, loader, device):
+    model.to(device)
+    model.eval()
+    targets, preds = gather_predictions(model, loader, device)
+    index = calculate_index_of_agreement(targets, preds)
+    mse, rmse = calculate_MSE_RMSE(targets, preds)
+    mae = calculate_MAE(targets, preds)
+    return mae, mse, rmse, index
+
+
+def gather_predictions(model, loader, device):
+    targets = np.array([])
+    preds = np.array([])
+
+    for data, target in loader:
+        targets = np.concatenate([targets, target.numpy()])
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        pred = torch.round(output, decimals=2)
+        preds = np.concatenate([preds, pred.squeeze().cpu().detach().numpy()])
+
+    return targets, preds
+
+
+def calculate_MSE_RMSE(targets, preds):
+    diff2 = (targets - preds)**2
+    mse = diff2.mean()
+    rmse = np.sqrt(mse)
+    return mse, rmse
+
+
+def calculate_index_of_agreement(targets, preds):
+    obs_mean = targets.mean()
+    numerator = (targets - preds)**2
+    denominator = (np.abs(preds - obs_mean) + np.abs(targets - obs_mean))**2
+    return 1 - (numerator.sum()/denominator.sum())
+
+
+def calculate_MAE(targets, preds):
+    return np.abs(targets-preds).mean()
